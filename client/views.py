f5328befdf808c8743e742c6d64c5dce54716ad3
hyang <eunhyang90218@gmail.com>
2017-02-02 07:33:43 +0900

    add logout

--- client/views.py
+++ client/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    # article_list = Article.objects.all()
+    # ctx = {
+    #     "article_list" : article_list
+    # }
+    return render(request, 'index.html', {})
--- partner/views.py
+++ partner/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.contrib.auth import (
+    authenticate, login as auth_login, logout as auth_logout
+    )
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def index(request):
+    ctx = {}
+    return render(request, "index.html", ctx)
+
+def login(request):
+    ctx = {}
+
+    if request.method == "GET":
+        pass
+    elif request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            print('----------------로그인 성공----------------')
+            auth_login(request, user)
+            return redirect("/partner/")
+        else:
+            print('----------------사용자 없음----------------')
+            ctx.update({"error" : "사용자가 없습니다."})
+
+    return render(request, "login.html", ctx)
+
+def logout(request):
+    auth_logout(request)
+    return redirect("/partner/")
+
+def signup(request):
+    if request.method == "GET":
+        pass
+    elif request.method == "POST":
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        # print(username, email, password)
+
+        user = User.objects.create_user(username, email, password)
+        # Article.objects.create(title="", content="")
+
+    ctx = {}
+    return render(request, "signup.html", ctx)
